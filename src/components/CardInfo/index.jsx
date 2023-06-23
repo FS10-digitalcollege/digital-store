073f86dc35070065095224a18587610e17933876
jvitor691 <jvitormarques2@hotmail.com>
2023-06-22 23:29:32 -0300

    page incompleta

--- src/components/CardInfo/index.jsx
+++ src/components/CardInfo/index.jsx
@@ -6,10 +6,10 @@ const CardInfo = () =>{
         <>
         <div className="card">
                 <h4>Informaçoes pessoais</h4>
-                <span>Nome:</span><p>João vitor</p>
-                <span>CPF:</span><p>000000000</p>
-                <span>Email:</span><p>fjkfjkfjkfjkj</p>
-                <span>Número:</span><p>455445454545</p>
+                <div className="info"><span>Nome:</span><p>João vitor</p></div>
+                <div className="info"><span>CPF:</span><p>8898900000</p></div>
+                <div className="info"><span>Celular:</span><p>(85)74748483</p></div>
+                <div className="info"><span>Email:</span><p>hjuhujgg@jgjh.com</p></div>
         </div>
         </>
     )
--- src/pages/ConclusaoDeCompra/index.jsx
+++ src/pages/ConclusaoDeCompra/index.jsx
@@ -6,11 +6,15 @@ import '../../components/CardInfo/style.css';
 const ConclusaoDeCompra = () =>{
     return(
         <>
-        <div className="container">
+        <div id="container">
+            <div className='cardImg'>
                 <img src="src\pages\ConclusaoDeCompra\assets\image.png" alt="compra realizada" />
                 <h1>Compra realizada com sucesso</h1>
-        </div>
+            </div>
         <CardInfo></CardInfo>
+        <CardInfo></CardInfo>
+        <CardInfo></CardInfo>
+        </div>
         </>
     )
 
