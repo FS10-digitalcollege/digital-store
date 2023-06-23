66c758c5f3d255c28769617df721acedeaa87ad4
jvitor691 <jvitormarques2@hotmail.com>
2023-06-23 20:25:19 -0300

    tela feita

--- src/pages/ConclusaoDeCompra/index.jsx
+++ src/pages/ConclusaoDeCompra/index.jsx
@@ -9,11 +9,28 @@ const ConclusaoDeCompra = () =>{
         <div id="container">
             <div className='cardImg'>
                 <img src="src\pages\ConclusaoDeCompra\assets\image.png" alt="compra realizada" />
-                <h1>Compra realizada com sucesso</h1>
+                <h1>Compra realizada com sucesso!</h1>
             </div>
+
         <CardInfo></CardInfo>
         <CardInfo></CardInfo>
         <CardInfo></CardInfo>
+
+        <div className='cardResumo'>
+            <h4>Resumo de Compra</h4>
+            <div className='cardResumoInfo'>
+                <img src="src\pages\ConclusaoDeCompra\assets\tenis.png" alt="tenis" srcset="" />
+                <p>Tênis Nike Revolution 6 Next Nature Masculino</p>
+            </div>
+        </div>
+        <div className='cardFooter'>
+            <h2>Total</h2>
+            <div>
+                <span>R$ 219,00</span>
+                <p>ou 10x de R$ 21,00 sem juros</p>
+            </div>
+        </div>
+        <a href="">Imprimir recibo</a>
         </div>
         </>
     )
